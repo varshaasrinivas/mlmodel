962bb53225a333d13e1f46e88dd26d18673011d8
varshaasrinivas <varshasriinivas@gmail.com>
2025-07-09 12:07:25 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+
+# Load data
+df = pd.read_csv('data/iris.csv')
+X = df.drop('Iris-setosa', axis=1)
+y = df['Iris-setosa']
+
+# Train model
+model =  KNeighborsClassifier()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, 'model/model.pkl')
+print(" Heart disease model trained.")
